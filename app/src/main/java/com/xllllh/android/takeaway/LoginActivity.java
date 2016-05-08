d2b1c3e7c01b5064794422ef62bffd26a9bcf858
0xLLLLH <linlihao159@gmail.com>
2016-05-08 23:44:57 +0800

    Add Login Page

---
+++ app/src/main/java/com/xllllh/android/takeaway/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.xllllh.android.takeaway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/xllllh/android/takeaway/MainActivity.java
+++ app/src/main/java/com/xllllh/android/takeaway/MainActivity.java
@@ -1,13 +1,27 @@
 package com.xllllh.android.takeaway;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = (Button) findViewById(R.id.login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
