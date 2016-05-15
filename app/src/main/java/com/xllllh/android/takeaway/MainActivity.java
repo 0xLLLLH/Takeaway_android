228a332b638f112d4af879a040fbacfe0c625926
0xLLLLH <linlihao159@gmail.com>
2016-05-15 23:12:51 +0800

    Modify Theme

--- app/src/main/java/com/xllllh/android/takeaway/MainActivity.java
+++ app/src/main/java/com/xllllh/android/takeaway/MainActivity.java
@@ -9,19 +9,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.viewpagerindicator.TitlePageIndicator;
-
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Set the pager with an adapter
-        ViewPager pager = (ViewPager)findViewById(R.id.pager);
-        //pager.setAdapter(new TestAdapter(getSupportFragmentManager()));
 
-        //Bind the title indicator to the adapter
-        TitlePageIndicator titleIndicator = (TitlePageIndicator)findViewById(R.id.titles);
-        titleIndicator.setViewPager(pager);
     }
 }
